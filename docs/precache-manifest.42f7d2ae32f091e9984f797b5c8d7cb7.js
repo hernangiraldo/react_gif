f5d6d64ecfcf788208648095f1d4400135ebfb11
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2020-06-30 19:49:52 -0400

    build

---
+++ docs/precache-manifest.42f7d2ae32f091e9984f797b5c8d7cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50c062f3cfeb0702bcd7d7c8b7267c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff067f7b5a61af6b192",
+    "url": "/static/css/main.96e75b82.chunk.css"
+  },
+  {
+    "revision": "dbac60613cd21329bee2",
+    "url": "/static/js/2.e67cf787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e67cf787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff067f7b5a61af6b192",
+    "url": "/static/js/main.360e60e4.chunk.js"
+  },
+  {
+    "revision": "2c7e0a9c18916f684e55",
+    "url": "/static/js/runtime-main.3083b03f.js"
+  }
+]);
